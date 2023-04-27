20652164762c1072eb34ba203d520126638f43df
Pallavi Surana <pallavi.surana.97@gmail.com>
2023-04-27 00:51:58 -0400

    v1.0
    Shiny App

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# Ct-Mark-Mut
-Mutation Data for marker genes of a Cell Type
+## Ct.Mark.Mut
+
+Ct-Mark-Mut is a Shiny app that allows users to explore mutations in marker genes for specific cell types in different species. The app provides access to a list of marker genes for a given cell type, fetched from the PanglaoDB database, and then allows the user to query the COSMIC mutation database for mutations in the selected marker genes.
+
+### Usage
+
+To use the app, follow these steps:
+
+1.  Choose the organ of interest from the drop-down list.
+
+2.  Choose the cell type corresponding to the selected organ.
+
+3.  Choose one or more species from the check-box options to display mutations for the selected species.
+
+4.  Set the number of pages you want to fetch from COSMIC. The number of mutations displayed will depend on the page limit.
+
+5.  Click the "Show me the mutations in the cell type markers" button to fetch mutations in the selected marker genes.
+
+6.  The app will display a table of the selected marker genes from PanglaoDB, followed by a table of mutations in the marker genes from COSMIC.
+
+7.  You can also download the tables by clicking the "Download Table" button.
+
+8.  Finally, a summary plot of the mutation burden is displayed at the bottom of the page.
+
+### Requirements
+
+The app requires the following R packages:
+
+-   httr
+
+-   jsonlite
+
+-   dplyr
+
+-   data.table
+
+-   shiny
+
+-   DT
+
+-   tidyr
+
+-   janitor
+
+-   scales
+
+-   ggplot2
+
+-   plotly
+
+### Acknowledgments
+
+This app is based on data from the following sources:
+
+The PanglaoDB database The COSMIC database
+
+### Contributors
+
+The app was developed by Pallavi Surana.
---
+++ app.R
@@ -0,0 +1,281 @@
+
+##----------------------------------------------------------------------------------------------------------------------------------
+##----------------------------------------------------------------------------------------------------------------------------------
+
+#-Idea of the shiny app
+#-1. Give the user options to choose cell type of interest
+#-2. Choose a species
+#-3. User sees a list of marker genes of that cell type of interest in table from panglaoDB
+#-4. fetch mutations landscape for marker genes and summary plot or table
+
+##----------------------------------------------------------------------------------------------------------------------------------
+##----------------------------------------------------------------------------------------------------------------------------------
+
+
+setwd("/Users/pallavisurana/Desktop/extra_codes/bmi530/final_app/")
+
+
+##----------------------------------------------------------------------------------------------------------------------------------
+##----------------------------------------------------------------------------------------------------------------------------------
+
+pacman::p_load(httr, jsonlite, dplyr, data.table, shiny, DT, tidyr, janitor, scales, ggplot2, plotly)
+
+
+##----------------------------------------------------------------------------------------------------------------------------------
+##----------------------------------------------------------------------------------------------------------------------------------
+
+panglaoDB_csv = fread("data/PanglaoDB_markers_27_Mar_2020.tsv")
+panglaoDB_csv = panglaoDB_csv %>% unique %>% drop_na() %>% clean_names()
+cto_all = data.frame(panglaoDB_csv$organ, panglaoDB_csv$cell_type) %>% unique
+colnames(cto_all) = c("organ", "cell_type")
+
+##----------------------------------------------------------------------------------------------------------------------------------
+##----------------------------------------------------------------------------------------------------------------------------------
+
+
+# Define UI
+ui <- fluidPage(
+  titlePanel(div("Ct-Mark-Mut", 
+                 tags$br(),
+                 tags$small("Mutation Data for marker genes of a Cell Type"))
+  ),
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      wellPanel(
+        ##-choose an organ 
+        selectInput(inputId = "organ1",
+                    label = "Select an organ:",
+                    choices = unique(cto_all$organ),
+                    selected = NULL),
+        
+        ##-choose cell type corresponding to the organ
+        selectInput(inputId = "cell_type1",
+                    label = "Select a cell type:",
+                    choices = NULL,
+                    selected = NULL),
+        
+        ##-choose organism of interest or all options available
+        checkboxGroupInput("speciesInput", label = "Select species for cell type:", 
+                           choices = list("Hs" = "Hs", "Mm" = "Mm"),
+                           selected = c("Hs","Mm"))
+      ),
+      
+      wellPanel(
+        ##-number of pages to show form COSMIC mutation database API call
+        sliderInput("pageInput", "Select page limit for Mutations table:", min = 1, max = 10, value = 2)
+      ),
+      
+      wellPanel(
+        ##-Only works when Run is clicked
+        actionButton("run", "Show me the mutations in the cell type markers")
+      ),
+    ),
+    
+    mainPanel(
+      h3("Marker Table from PanglaoDB for cell type of interest"),  
+      DTOutput("markerTable"),
+      br(), 
+      downloadButton("downloadMarkerTable", label = "Download Table"),
+      br(),
+      br(),
+      h3("Mutation Table from COSMIC"),  
+      DTOutput("mutationTable"),
+      br(), 
+      downloadButton("downloadMutationTable", label = "Download Table") ,
+      br(),
+      br(), 
+      plotlyOutput("site_burden_plot"),
+      br(),
+      br()
+    )
+  )
+)
+
+##----------------------------------------------------------------------------------------------------------------------------------
+##----------------------------------------------------------------------------------------------------------------------------------
+
+
+# Define server 
+server <- function(input, output, session) {
+  
+  ##-pick the organ and cell type of interest from user
+  observeEvent(input$organ1, {
+    choices <- cto_all$cell_type[cto_all$organ == input$organ1]
+    updateSelectInput(session, "cell_type1", choices = choices)
+  })
+
+  
+  observeEvent(input$run, {
+    
+    ##-get cell type marker information from panglaoDb tsv file
+    markers <- reactive({
+      x = panglaoDB_csv  %>% subset(organ == input$organ1) %>% subset(cell_type == input$cell_type1)
+      
+      ##-take inot account the species that the user picks for the cell types markers information
+      if("Mm" %in% input$speciesInput && "Hs" %in% input$speciesInput){
+        x <- x %>% subset(species %in% c("Mm Hs"))
+      }else if("Mm" %in% input$speciesInput){
+        x <- x %>% subset(species == "Mm")
+      }else if("Hs" %in% input$speciesInput){
+        x <- x %>% subset(species == "Hs")
+      }
+      
+      x
+    })
+    
+    
+    ##-genes to query from COSMIC
+    gene_query1 <- reactive({
+      markers()$official_gene_symbol
+    })
+    
+    
+    ##- used the COSMIC v4 API here
+    ##- query data for one gene from cosmic
+    COSMIC_query <- list(
+      format = 'json',
+      maxList = 500,
+      df = 'MutationID,GeneName,MutationCDS,MutationAA,AccessionNumber,PrimaryHistology,PrimarySite,PubmedPMID',
+      page = 1,
+      page_size = 2000
+    )
+    
+    
+    ##-fetch data using cosmic api
+    fetch_data <- function(gene_nms, page_limit = Inf) {
+      all_data <- data.frame()  # initialize an empty data frame
+      for (gene in gene_nms) {
+        COSMIC_query$terms <- gene
+        COSMIC_query$page_size <- 500  # set the page size to the maximum
+        page <- 1
+        while (TRUE) {
+          if (page > page_limit) {
+            break  # stop if the page limit has been reached
+          }
+          COSMIC_query$page <- page
+          response <- GET('https://clinicaltables.nlm.nih.gov/api/cosmic/v4/search', query = COSMIC_query)
+          data <- content(response, as = "text") %>% fromJSON()
+          data.df <- data[[4]] %>% as.data.frame()
+          if (nrow(data.df) == 0) {
+            break  # stop if there are no more results
+          }
+          colnames(data.df) <- strsplit(COSMIC_query$df, ",")[[1]]
+          all_data <- rbind(all_data, data.df)  # combine the data frames
+          page <- page + 1
+        }
+      }
+      return(all_data)
+    }
+    
+    
+    ##-COSMIC mutation data for cell type markers
+    cosmic_df <- reactive({
+      fetched_df = fetch_data(gene_query1(), page_limit = input$pageInput)
+      fetched_df = fetched_df[!grepl(";", fetched_df$PrimarySite), ]
+      fetched_df = fetched_df[!grepl(";", fetched_df$PrimaryHistology), ]
+      
+      ##-compute mutation burden for primary site
+      mutation_burden <- fetched_df %>%
+        group_by(PrimarySite) %>%
+        summarise(mut_per_mb_Site = n() / 1000000) %>%
+        arrange(mut_per_mb_Site) %>%
+        mutate(PrimarySite = forcats::fct_reorder(PrimarySite, mut_per_mb_Site))
+      
+      ##-merge mutation burden and cosmic fetched data
+      merged_Df = merge(fetched_df, mutation_burden, by = "PrimarySite")
+      merged_Df$PubmedPMID = NULL
+    
+      
+      merged_Df$mut_per_mb_Site = -log10(merged_Df$mut_per_mb_Site)
+      merged_Df
+
+    })
+    
+    
+    
+    output$mutationTable <- renderDT({
+      if (nrow(cosmic_df()) > 0) {
+        cosmic_df() %>%
+          lapply(function(x) ifelse(nchar(as.character(x)) > 20, paste0(substr(x, 1, 20), "..."), x)) %>%
+          as.data.frame() %>%
+          DT::datatable(., options = list(scrollX = TRUE, scrollY = "200px", pageLength = -1, dom = 't'))
+      } else {
+        stop("No mutation data found for marker genes for organism of interest")
+      }
+    })
+    
+    
+    output$markerTable <- renderDT({
+      if (nrow(markers()) > 0) {
+        markers() %>%
+          select_if(~is.character(.)) %>%
+          lapply(function(x) ifelse(nchar(as.character(x)) > 20, paste0(substr(x, 1, 20), "..."), x)) %>%
+          as.data.frame() %>%
+          DT::datatable(., options = list(scrollX = TRUE, scrollY = "200px", pageLength = -1, dom = 't'))
+      } else {
+        stop("No markers found for cell type chosen in the organism of interest")
+      }
+    })
+    
+    output$downloadMarkerTable <- downloadHandler(
+      filename = function() {
+        paste0("cell-type_marker_table_", Sys.Date(), ".csv")
+      },
+      content = function(file) {
+        write.csv(markers(), file, row.names = FALSE)
+      }
+    )
+    
+    output$downloadMutationTable <- downloadHandler(
+      filename = function() {
+        paste0("mutations_table_", Sys.Date(), ".csv")
+      },
+      content = function(file) {
+        write.csv(cosmic_df(), file, row.names = FALSE)
+      }
+    )
+    
+
+    
+    ##-mutation burden plot
+    output$site_burden_plot <- renderPlotly({
+      
+      ##-plot for primary site
+      p <- ggplot(cosmic_df(), aes(x = reorder(PrimarySite, -mut_per_mb_Site), y = mut_per_mb_Site)) +
+        geom_col(fill = "steelblue") +
+        labs(title = "Mutation Burden by Primary Site",
+             x = "Primary Site",
+             y = "Mutations per Megabase (-log10)") +
+        theme(plot.title = element_text(size = 18),
+              axis.title = element_text(size = 14),
+              panel.background = element_blank(),
+              panel.grid.major = element_line(colour = "gray90"),
+              panel.grid.minor = element_blank(),
+              axis.line = element_line(colour = "black"),
+              axis.ticks = element_line(colour = "black")) 
+        theme_classic()
+      
+      ##-convert to plotly
+      ggplotly(p) %>% layout(xaxis = list(tickangle = 300))  
+      
+    })
+    
+  })
+}
+
+  
+##----------------------------------------------------------------------------------------------------------------------------------
+##----------------------------------------------------------------------------------------------------------------------------------
+  
+  
+##-Run the app
+shinyApp(ui, server)
+  
+  
+##----------------------------------------------------------------------------------------------------------------------------------
+##----------------------------------------------------------------------------------------------------------------------------------
+
+
+  
